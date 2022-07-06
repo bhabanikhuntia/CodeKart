8b61e6b756d3d3dbbf2a18514d5fb3704b52cc8c
bhabanikhuntia <96862165+bhabanikhuntia@users.noreply.github.com>
2022-07-06 16:43:13 +0530

    text commit

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +9,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
+
 <body>
-    Hello World
+   <?php include("./src/Header.php"); ?>
+   <?php include("./src/Footer.php"); ?>
 </body>
+
 </html>
